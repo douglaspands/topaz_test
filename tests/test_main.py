b117ddf81de241c04d87e41285a0ee6cf6d8aa08
Douglas Panhota <douglaspands@gmail.com>
2021-05-21 12:08:09 -0300

    melhoria no lint

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,10 +10,10 @@ config.FILE_OUTPUT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "files", "output.txt")
 )
 
-from topaz_test import __main__
-
 
 def test_main_1_success():
+
+    from topaz_test import __main__
 
     expect_rc = 0
     rc = __main__.main()
@@ -22,10 +22,9 @@ def test_main_1_success():
 
 def test_main_2_fail():
 
+    from topaz_test import __main__
+
     expect_rc = 1
-    with patch(
-        "topaz_test.__main__.ServerManager.new_task_for_each_user",
-        side_effect=Exception("mocked error"),
-    ) as mock:
+    with patch("topaz_test.__main__.ServerManager.new_task_for_each_user", side_effect=Exception("mocked error")):
         rc = __main__.main()
     assert rc == expect_rc
